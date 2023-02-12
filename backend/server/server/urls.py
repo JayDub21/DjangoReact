c30a22f64d6511e8ae1322f24248902a6fd223d7
Justin Wofford <justin.wofford.21@gmail.com>
2023-02-11 19:55:07 -0600

    react static file settings

--- backend/server/server/urls.py
+++ backend/server/server/urls.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.views.generic import TemplateView
 
 from apps.accounts.urls import accounts_urlpatterns
+
+# Admin Page Title & Sub-title
+admin.site.site_header = 'DjangoReact Admin'
+admin.site.index_title = 'Admin Dashboard'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
 urlpatterns += accounts_urlpatterns # add URLs for authentication
+
+urlpatterns += [re_path('(^(?!(api|admin)).*$)',
+                        TemplateView.as_view(template_name='index.html'))]
